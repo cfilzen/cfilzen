483f8e2ecd7c2cd32d318746ef03a8c5232232a7
cfilzen <cfilzen@gmail.com>
2022-05-04 16:55:16 -0400

    Converting project to Web Assembly

--- ChrisFilzenBlazor/Client/Program.cs
+++ ChrisFilzenBlazor/Client/Program.cs
@@ -1,14 +1,11 @@
-using CFilzenMain;
+using ChrisFilzenBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Havit.Blazor.Components.Web;
-using Havit.Blazor.Components.Web.Bootstrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddHxServices();
 
 await builder.Build().RunAsync();
