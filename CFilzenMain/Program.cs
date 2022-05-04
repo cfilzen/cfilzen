3a2a7e00c3fc63351a1de8f9635790826661bdc3
cfilzen <cfilzen@gmail.com>
2022-05-04 17:16:59 -0400

    Revert "Converting project to Web Assembly"
    This reverts commit 483f8e2ecd7c2cd32d318746ef03a8c5232232a7.

--- CFilzenMain/Program.cs
+++ CFilzenMain/Program.cs
@@ -1,11 +1,14 @@
-using ChrisFilzenBlazor.Client;
+using CFilzenMain;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHxServices();
 
 await builder.Build().RunAsync();
