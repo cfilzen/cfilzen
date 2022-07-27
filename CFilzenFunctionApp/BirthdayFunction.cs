1639bc9ef59fa90e7c32b1fb500560feb36cd4dd
cfilzen <cfilzen@gmail.com>
2022-07-26 23:16:01 -0400

    Adding birthday logic

--- CFilzenFunctionApp/BirthdayFunction.cs
+++ CFilzenFunctionApp/BirthdayFunction.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -19,16 +20,31 @@ namespace CFilzenFunctionApp
             log.LogInformation("We translated birthday to age.");
 
             string name = req.Query["name"];
+            string birthdate = req.Query["birthdate"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+            birthdate = birthdate ?? data?.birthdate;
+
+            //"08/08/2020" format
+            var age = CalculateAge(DateTime.Parse(birthdate));
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. You told me your birthday is {birthdate}. Your age is {age}..";
 
             return new OkObjectResult(responseMessage);
+        }
+
+        private static int CalculateAge(DateTime dateOfBirth)
+        {
+            var age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < dateOfBirth.DayOfYear)
+                age = age - 1;
+
+            return age;
         }
     }
 }
---
+++ CFilzenFunctionApp/Types/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CFilzenFunctionApp.Types
+{
+    public class PersonRequest
+    {
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+    }
+
+    public class PersonResponse
+    {
+        public int Age { get; set; }
+    }
+}
